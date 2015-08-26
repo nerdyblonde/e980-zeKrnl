d05ea850bee33b0c3e8161a9e26317f2200c0b4d
Igor <c.igor@gmx.com>
2015-08-27 00:10:11 +0200

    Adding support for fastcharge

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -50,6 +50,7 @@
 #include <mach/msm_bus.h>
 #include <mach/rpm-regulator.h>
 
+<<<<<<< HEAD
 #ifdef CONFIG_LGE_PM
 #include <mach/restart.h>
 #include <linux/reboot.h>
@@ -68,6 +69,12 @@
 #include "../../../sound/soc/codecs/wcd9310.h"
 #endif /*CONFIG_LGE_AUX_NOISE*/
 
+=======
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
+>>>>>>> 18eb498... fastcharge: initial adaptation for Nexus 4
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -1253,6 +1260,11 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 		dev_err(motg->phy.dev,
 			"Failed notifying %d charger type to PMIC\n",
 							motg->chg_type);
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge > 0)
+		mA = IDEV_ACA_CHG_MAX;
+#endif
 
 	if (motg->cur_power == mA)
 		return;
