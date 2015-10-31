d46ba97a5a1a979f5a4395611b7ba8fc0315c3b9
Igor <ShadySquirrel@users.noreply.github.com>
2015-10-31 10:03:38 +0100

    cpufreq: add brazilianwax

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -405,6 +405,9 @@ extern struct cpufreq_governor 		cpufreq_gov_lulzactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
 extern struct cpufreq_governor cpufreq_gov_ondemandx;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BRAZILIANWAX)
+extern struct cpufreq_governor cpufreq_gov_brazilianwax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_brazilianwax)
 #endif
 
 
